6f76346ebd477b03197b1f0566b4714d84ac075f
Paradoks <Paradoks92@mail.ru>
2024-11-08 13:19:09 +0300

    v2

--- js/cart-01.js
+++ js/cart-01.js
@@ -1,25 +1,24 @@
 // div внутри корзины в который мы добавляем товары
 const cartWrapper = document.querySelector(".cart-wrapper");
 //отслеживаем клик на странице
-window.addEventListener("click", function(event){
+window.addEventListener("click", function (event) {
+  //Проверка что клик сделан по кнопке добавить в корзину
+  if (event.target.hasAttribute("data-cart")) {
+    //Находим карточку товара внутри которой был сделан клик
+    const card = event.target.closest(`.card`);
+    // Соберем все данные в одну пепеременную
+    const productInfo = {
+      id: card.dataset.id,
+      imgSrc: card.querySelector(`.product-img`).getAttribute("src"),
+      title: card.querySelector(`.item-title`).innerText,
+      itemsInbox: card.querySelector(`[data-items-in-box]`).innerText,
+      weight: card.querySelector(`.price__weight`).innerText,
+      price: card.querySelector(`.price__currency`).innerText,
+      counter: card.querySelector(`[data-counter]`).innerText,
+    };
 
-    //Проверка что клик сделан по кнопке добавить в корзину
-    if (event.target.hasAttribute('data-cart')){
-        //Находим карточку товара внутри которой был сделан клик
-        const card = event.target.closest(`.card`)
-        // Соберем все данные в одну пепеременную
-        const productInfo = {
-            id: card.dataset.id,
-            imgSrc: card.querySelector(`.product-img`).getAttribute("src"),
-            title: card.querySelector(`.item-title`).innerText,
-            itemsInbox: card.querySelector(`[data-items-in-box]`).innerText,
-            weight: card.querySelector(`.price__weight`).innerText,
-            price: card.querySelector(`.price__currency`).innerText,
-            counter: card.querySelector(`[data-counter]`).innerText,
-        };
-
-        //Подставим в шаблон собранные данные
-        const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
+    //Подставим в шаблон собранные данные
+    const cartItemHTML = `<div class="cart-item" data-id="${productInfo.id}">
 								<div class="cart-item__top">
 									<div class="cart-item__img">
 										<img src="${productInfo.imgSrc}" alt="">
@@ -47,9 +46,7 @@ window.addEventListener("click", function(event){
 									</div>
 								</div>
 							</div>`;
-        //отобразим товар в корзине
-        cartWrapper.insertAdjacentHTML(`beforeend`, cartItemHTML);
-    
-    }
+    //отобразим товар в корзине
+    cartWrapper.insertAdjacentHTML(`beforeend`, cartItemHTML);
+  }
 });
-
